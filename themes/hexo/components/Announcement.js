21a5cd903d5868507eecb0e3f4a8a980c929af5f
GooGACKnight <kmkopenai@gmail.com>
2025-03-25 00:18:32 +0800

    Additional notes

--- themes/hexo/components/Announcement.js
+++ themes/hexo/components/Announcement.js
@@ -1,21 +1,43 @@
+/*
+ * @Author: GooGACKnight kmkopenai@gmail.com
+ * @Date: 2025-03-22 09:38:00
+ * @LastEditors: GooGACKnight kmkopenai@gmail.com
+ * @LastEditTime: 2025-03-25 00:03:42
+ * @FilePath: \NotionNext\themes\hexo\components\Announcement.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 导入全局状态管理器和动态导入功能
 import { useGlobal } from '@/lib/global'
 import dynamic from 'next/dynamic'
 
+// 动态导入NotionPage组件以提高性能
 const NotionPage = dynamic(() => import('@/components/NotionPage'))
 
+/**
+ * Announcement组件用于显示公告信息
+ * @param {Object} post - 包含公告内容的对象
+ * @param {string} className - 要添加到组件外部div的类名
+ * @returns {JSX.Element} - 公告组件的JSX
+ */
 const Announcement = ({ post, className }) => {
+  // 使用全局状态管理器获取当前语言环境
   const { locale } = useGlobal()
+
+  // 如果post对象包含blockMap属性，则渲染公告内容
   if (post?.blockMap) {
     return <div className={className}>
         <section id='announcement-wrapper' className="dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray">
             <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>
             {post && (<div id="announcement-content">
-            <NotionPage post={post} className='text-center' />
-        </div>)}
+                <NotionPage post={post} className='text-center' />
+            </div>)}
         </section>
     </div>
   } else {
+    // 如果post对象不包含blockMap属性，则不渲染任何内容
     return <></>
   }
 }
+
+// 导出Announcement组件以供其他组件使用
 export default Announcement
--- themes/hexo/components/CategoryGroup.js
+++ themes/hexo/components/CategoryGroup.js
@@ -1,30 +1,43 @@
 import Link from 'next/link'
 
+/**
+ * CategoryGroup 组件用于显示一个分类列表，其中每个分类都是可点击的链接。
+ * 它根据当前分类和所有分类生成一个分类列表，用户可以点击列表中的分类来浏览相应的内容。
+ * 
+ * @param {Object} props - 组件的props对象。
+ * @param {string} props.currentCategory - 当前选中的分类名称。
+ * @param {Array} props.categories - 包含所有分类的数组。
+ * @returns {JSX.Element} - 返回一个分类列表，如果categories参数为null，则不返回任何内容。
+ */
 const CategoryGroup = ({ currentCategory, categories }) => {
+  // 如果没有提供categories数组，则不渲染任何内容。
   if (!categories) {
     return <></>
   }
-  return <>
-    <div id='category-list' className='dark:border-gray-600 flex flex-wrap  mx-4'>
-      {categories.map(category => {
-        const selected = currentCategory === category.name
-        return (
-          <Link
-            key={category.name}
-            href={`/category/${category.name}`}
-            passHref
-            className={(selected
-              ? 'hover:text-white dark:hover:text-white bg-indigo-600 text-white '
-              : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
-              '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
-
-            <div> <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name}({category.count})</div>
-
-          </Link>
-        );
-      })}
-    </div>
-  </>;
+  // 渲染分类列表。
+  return <div id='category-list' className='dark:border-gray-600 flex flex-wrap  mx-4'>
+    {categories.map(category => {
+      // 判断当前分类是否被选中。
+      const selected = currentCategory === category.name
+      // 为每个分类生成一个可点击的链接。
+      return (
+        <Link
+          key={category.name}
+          href={`/category/${category.name}`}
+          passHref
+          // 根据分类是否被选中，应用不同的样式。
+          className={(selected
+            ? 'hover:text-white dark:hover:text-white bg-indigo-600 text-white '
+            : 'dark:text-gray-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-indigo-600') +
+            '  text-sm w-full items-center duration-300 px-2  cursor-pointer py-1 font-light'}>
+          <div>
+            // 使用图标表示分类是否被选中，并显示分类名称和包含的内容数量。
+            <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category.name}({category.count})
+          </div>
+        </Link>
+      );
+    })}
+  </div>
 }
 
 export default CategoryGroup
